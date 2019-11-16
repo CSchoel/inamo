8f626bd46b675f2107b9f2c13850dad8f6b8a3a6
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2019-11-16 12:54:57 +0100

    updates deploy script to also copy reference code if needed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+# copies modelica files into deploy folder
+# must be run from within this folder
 
 TARGET=../../modelica-deploy/InaMo
+REF=../../modelica-deploy/InaMo-ref
 
 if [ -d $TARGET ]; then
   echo "Removing $TARGET ..."
-  rm -rf $TARGET
+  rm -rf "$TARGET"
 fi
 
 echo "Copying all files from ../InaMo to $TARGET ..."
-cp -r ../InaMo $TARGET
+cp -r ../InaMo "$TARGET"
+
+if [ ! -d $REF ]; then
+  echo "Copying reference code to $REF ..."
+  mkdir -p "$REF"
+  cp -r ../references/2019-kupferschmitt-inada2009/modelica/* "$REF"
+fi
 
 echo "Done."
