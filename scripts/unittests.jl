6bd078315ae3f7a97b58bb3523db16b63855eb3c
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-02-05 14:30:11 +0100

    adds julia script with unittests

---
+++ scripts/unittests.jl
@@ -0,0 +1,48 @@
+using OMJulia
+using Base.Filesystem
+using Test
+
+moroot = dirname(@__DIR__)
+outdir = "out"
+cd(moroot)
+if !ispath(outdir)
+    mkdir(outdir)
+end
+cd(outdir)
+
+omc = OMJulia.OMCSession()
+try
+    mopath = OMJulia.sendExpression(omc, "getModelicaPath()")
+    mopath = "$mopath:$(escape_string(moroot))"
+    println("Setting MODELICAPATH to ", mopath)
+    OMJulia.sendExpression(omc, "setModelicaPath(\"$mopath\")")
+    # load Modelica standard library
+    OMJulia.sendExpression(omc, "loadModel(Modelica)")
+    @testset "Simulate examples" begin
+        @testset "SodiumChannelSteady" begin
+            r = OMJulia.sendExpression(omc, "loadModel(InaMo.Examples.SodiumChannelSteady)")
+            @test r
+            es = OMJulia.sendExpression(omc, "getErrorString()")
+            @test es == ""
+            r = OMJulia.sendExpression(omc, "simulate(InaMo.Examples.SodiumChannelSteady, stopTime=80, numberOfIntervals=8000, outputFormat=\"csv\")")
+            @test !occursin("| warning |", r["messages"])
+            es = OMJulia.sendExpression(omc, "getErrorString()")
+            @test es == ""
+        end
+        @testset "SodiumChannelIV" begin
+            r = OMJulia.sendExpression(omc, "loadModel(InaMo.Examples.SodiumChannelIV)")
+            @test r
+            es = OMJulia.sendExpression(omc, "getErrorString()")
+            @test es == ""
+            r = OMJulia.sendExpression(omc, "simulate(InaMo.Examples.SodiumChannelIV, stopTime=80, numberOfIntervals=80000, outputFormat=\"csv\")")
+            @test !occursin("| warning |", r["messages"])
+            es = OMJulia.sendExpression(omc, "getErrorString()")
+            @test es == ""
+        end
+    end
+finally
+    println("Closing OMC session")
+    sleep(1)
+    OMJulia.sendExpression(omc, "quit()", parsed=false)
+    println("Done")
+end
