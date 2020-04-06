1f461b4173a9be2e4b844a30e0b2125ae2f741fb
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-04-06 22:40:11 +0200

    comments out debug lines

--- scripts/plot_validation.py
+++ scripts/plot_validation.py
@@ -433,8 +433,8 @@ def inada2009_S6A(fname):
     ax2.set_ylabel("ratio of molecules in state [1]")
     ax2.legend(loc="best")
     for a, b in [(i, i % 4 + 1) for i in range(1, 5)] + [(i % 4 + 1, i) for i in range(1, 5)]:
-        if a != 1 or b != 4:
-            continue
+        # if a != 1 or b != 4:
+        #   continue
         ax3.plot(
             data["time"] * 1000,
             data["naca.k_{}{}".format(a, b)],
