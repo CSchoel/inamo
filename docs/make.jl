fe9dba4534a8304c73a86a49680001901dfe0ebc
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-09-08 18:08:06 +0200

    adds documentation to travis CI script and adds deploydocs call in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,9 +6,7 @@ makedocs(
     format = Documenter.HTML()
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/CSchoel/inamo.git",
+    versions = ["v^", "v#.#", "stable" => "v^"]
+)
