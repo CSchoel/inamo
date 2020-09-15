59b6b6adbfd3e25da5eebfe8e71a3cc5b74dae63
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-09-15 16:54:46 +0200

    adds files and script to obtain true reference plot data (WIP)

---
+++ scripts/reconstruct_plot_data.py
@@ -0,0 +1,54 @@
+# uses data from hand-drawn SVG images to reconstruct plot data
+
+import numpy as np
+import lxml.etree as et
+import svgpathtools as svgpath
+
+
+def convert_path(path, x0, xfactor, y0, yfactor, steps):
+    p = svgpath.parse_path(path)
+    res = []
+    for i in range(steps):
+        t = i / (steps-1)
+        res.append(p.point(t))
+    return res
+
+
+def first_point(path):
+    return svgpath.parse_path(path).start
+
+
+def width(path):
+    p = svgpath.parse_path(path)
+    return abs(p.point(0)[0] - p.point(1)[0])
+
+
+def path_by_id(dom, id):
+    return dom.xpath("//path[@id = '{}']".format(id))[0].get("d")
+
+
+def reconstruct_full_cells_S7(fname):
+    # data from img/inada_orig_cells_discrete.svg
+    dom = et.parse(fname)
+    ylim_v = (
+        (first_point(path_by_id(dom, "voltage_low"))[1], -80),
+        (first_point(path_by_id(dom, "voltage_high"))[1], 40)
+    )
+    ylim_c = (
+        (first_point(path_by_id(dom, "ca_low"))[1], 0),
+        (first_point(path_by_id(dom, "ca_high"))[1], 1)
+    )
+    x0s = [first_point(path_by_id(dom, "x0_"+x))[0] for x in ["am", "an", "n", "nh"]]
+    xfactor = width(path_by_id(dom, "xbar")) / 50
+    print(ylim_v)
+    print(ylim_c)
+    print(x0s)
+    print(xfactor)
+    ylim_v = ((112.59237, -80), (49.120929, 40))
+    ylim_c = ((196.29762, 0), (122.3152, 1))
+    x0s = [-102.12249, 8.5055034, 111.50644, 214.79407]
+    xfactor = 23.99138 / 50
+
+
+if __name__ == "__main__":
+    reconstruct_full_cells_S7("img/inada_orig_cells_discrete.svg")
