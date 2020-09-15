ea27216f39fdcd716cbf6a78fc927de3295c3045
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-09-15 17:05:18 +0200

    finishes implementation of svg path extraction

--- scripts/reconstruct_plot_data.py
+++ scripts/reconstruct_plot_data.py
@@ -15,16 +15,22 @@ def convert_path(path, x0, xfactor, y0, yfactor, steps):
 
 
 def first_point(path):
-    return svgpath.parse_path(path).start
+    s = svgpath.parse_path(path).start
+    s = (s.real, s.imag)
+    return s
 
 
 def width(path):
     p = svgpath.parse_path(path)
-    return abs(p.point(0)[0] - p.point(1)[0])
+    return abs(p.point(0).real - p.point(1).real)
 
 
 def path_by_id(dom, id):
-    return dom.xpath("//path[@id = '{}']".format(id))[0].get("d")
+    ns = {"svg": "http://www.w3.org/2000/svg"}
+    path = dom.xpath(
+        "//svg:path[@id = '{}']".format(id), namespaces=ns
+    )[0].get("d")
+    return path
 
 
 def reconstruct_full_cells_S7(fname):
@@ -38,7 +44,10 @@ def reconstruct_full_cells_S7(fname):
         (first_point(path_by_id(dom, "ca_low"))[1], 0),
         (first_point(path_by_id(dom, "ca_high"))[1], 1)
     )
-    x0s = [first_point(path_by_id(dom, "x0_"+x))[0] for x in ["am", "an", "n", "nh"]]
+    x0s = [
+        first_point(path_by_id(dom, "x0_"+x))[0]
+        for x in ["am", "an", "n", "nh"]
+    ]
     xfactor = width(path_by_id(dom, "xbar")) / 50
     print(ylim_v)
     print(ylim_c)
