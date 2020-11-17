cf3bcece4b9af412ff088f905cbe0c7604c5b9ef
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-11-17 19:55:19 +0100

    adds debug_plot script for quick comparison between reference data and current simulation data

---
+++ scripts/debug_plot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas
+from pathlib import Path
+
+def debug_plot(model, variables):
+    curpath = Path("out") / "{}_res.csv".format(model)
+    refpath = Path("regRefData") / "{}_res.csv".format(model)
+    curdata = pandas.read_csv(curpath)
+    refdata = pandas.read_csv(refpath)
+    if variables is None:
+        variables = refdata.columns
+    colors = ["C{}".format(i) for i in range(1, 10)]
+    for v, c in zip(variables, colors):
+        if v.startswith("Δ"):
+            v = v[1:]
+            curiter = np.interp(refdata["time"], curdata["time"], curdata[v])
+            delta = curiter - refdata[v]
+            plt.plot(refdata["time"], delta, color=c, label="Δ{}".format(v))
+        else:
+            plt.plot(refdata["time"], refdata[v], linestyle="-", color="black", label="{} (ref)".format(v))
+            plt.plot(curdata["time"], curdata[v], linestyle="-", color=c, label="{} (cur)".format(v))
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    debug_plot("InaMo.Examples.SodiumChannelIV", ["Δcd"])
