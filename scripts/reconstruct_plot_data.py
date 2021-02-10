20fb33c0a5b9a3d1cb5aa316a9a2eae60611da49
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-02-10 02:10:05 +0100

    adds reconstruction plot for S2D

--- scripts/reconstruct_plot_data.py
+++ scripts/reconstruct_plot_data.py
@@ -361,5 +361,10 @@ if __name__ == "__main__":
     reconstruct_generic(
         "img/to_inada2009_S2C_orig.svg",
         xlim=(-120, 80), ylim=(0, 250),
+        xlabel="voltage [mV]", ylabel="time constant [ms]"
+    )
+    reconstruct_generic(
+        "img/to_inada2009_S2D_orig.svg",
+        xlim=(-120, 80), ylim=(0, 4000),
         xlabel="voltage [mV]", ylabel="time constant [ms]", debug_plot=True
     )
